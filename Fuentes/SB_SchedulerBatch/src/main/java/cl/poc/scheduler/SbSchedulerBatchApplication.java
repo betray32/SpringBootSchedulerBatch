90227631d0988aadb6261845117de3d84ea6d88c
Camilo Contreras <contreras.camilo32@gmail.com>
2019-08-01 18:41:47 -0400

    Carga inicial

---
+++ Fuentes/SB_SchedulerBatch/src/main/java/cl/poc/scheduler/SbSchedulerBatchApplication.java
@@ -0,0 +1,13 @@
+package cl.poc.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSchedulerBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSchedulerBatchApplication.class, args);
+	}
+
+}
---
+++ Fuentes/SB_SchedulerBatch/src/test/java/cl/poc/scheduler/SbSchedulerBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.poc.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbSchedulerBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
